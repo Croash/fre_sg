3976823f9225de68963b91391952473655619a97
shaozelin <1327914853@qq.com>
2022-02-23 08:59:29 +0800

    build basic stream

--- src/fiber/reconcilerBase.js
+++ src/fiber/reconcilerBase.js
@@ -9,12 +9,50 @@ import { getParentNode } from './getParentNode'
 import { Either, Left, Right } from '../functor'
 
 let preCommit = null
+let WIP = null
 
-export const reconcileLoopBase = compose(
-  (a, WIP) => {
-    console.log(a, WIP)
-    return WIP
+const updateHook = (WIP) => {
+  console.log('WIP_CHILDREN', WIP)
+  return WIP
+}
+
+export const reconcile = compose(
+  Either(
+    compose(WIP => {
+      // trampoline tail recurse how to work ???
+      // const reconcileLoop = compose((WIP) => WIP)
+      // WIP = trampoline(curry(reconcileLoop)(WIP))()
+      return WIP
+    }),
+    compose(WIP => WIP),
+  ),
+  (WIP) => {
+    WIP.parentNode = getParentNode(WIP)
+    isFn(WIP.type) ? updateHOOK(WIP) : updateHOOK(WIP)
+    WIP.dirty = WIP.dirty ? false : 0
+    WIP.oldProps = WIP.props
+    // 这里就push吗，不太理解
+    pushCommitItem(WIP)
+    return WIP.child ? Right.of(WIP.child) : Left.of(WIP)
   }
+)
+
+export const reconcileWorkLoop = compose(
+  Either(
+    compose(
+      (WIP) => WIP,
+    ),
+    compose(
+      consoleFunc('rightFunc'),
+      // reconcile
+      (WIP) => WIP,
+    ),
+  ),
+  ( didout, WIP ) => {
+    const goonWork = !shouldYield() || didout
+    console.log('didout, WIP ', didout, WIP )
+    return (goonWork && WIP) ? Right.of(WIP) : Left.of(WIP)
+  },
 )
 
 // const reconcileBase = curry()
@@ -24,20 +62,20 @@ export const reconcileWork = compose(
   Either(
     compose(
       () => {
-        preCommit && true // commit(preCommit)
+        if(preCommit) pushUpdateItem(preCommit)
         return null
       }
     ),
     compose(rcWork => rcWork)
   ),
   ({ didout, newWIP }) => {
-    console.log('fs123',!didout , newWIP)
-    return (!didout && newWIP) ? Right.of(reconcileWork) : Left.of(null)
+    const notOut = shouldYield() || didout
+    console.log((!notOut && newWIP))// ?? not sure
+    return (!notOut && newWIP) ? Right.of(reconcileWork) : Left.of(null)
   },
   (didout) => {
-    const WIP = shiftUpdateItem()._value
-    // trampoline change to closure
-    const newWIP = trampoline(curry(reconcileLoopBase)(didout))(WIP)
+    if(!WIP) WIP = shiftUpdateItem()._value
+    const newWIP = trampoline(curry(reconcileWorkLoop)(didout))(WIP)
     return { didout, newWIP }
   },
   consoleFunc('didoutM'),
