82bec42840f4eb8dde3caf04e170881c4f93ba60
shaozelin <1327914853@qq.com>
2024-03-28 16:36:02 +0800

    feat: github-demo udpate

--- apps/github-demo/rollup.dev.config.js
+++ apps/github-demo/rollup.dev.config.js
@@ -1,31 +1,61 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+// import typescript from '@rollup/plugin-typescript';
 import serve from 'rollup-plugin-serve';
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 import livereload from 'rollup-plugin-livereload';
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.js',
   output: {
-    file: 'dist/bundle.js',
-    format: 'iife',
+    file: 'dist/index.js',
+    format: 'cjs',
     sourcemap: true,
   },
   plugins: [
-    resolve(),
+    resolve({
+      alias: {
+        'react': 'frc'
+      }
+    }),
     commonjs(),
-    typescript(),
+    // typescript(),
     babel({
       babelHelpers: 'bundled',
-      presets: ['@babel/preset-react'],
-      exclude: /node_modules/,
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: {
+              "node": "current"
+            },
+            modules: false
+          }
+        ],
+        // "@babel/preset-typescript",
+        // "@babel/preset-react"
+      ],
+      plugins: [
+        [
+          "@babel/plugin-transform-react-jsx",
+          {
+            pragma: "h"
+          }
+        ]
+      ],
+      extensions: ['.js', '.jsx']
     }),
     serve({
       open: true,
       contentBase: ['dist'],
       port: 8080,
     }),
-    livereload(),
+    htmlTemplate({
+      template: './index.html',
+      target: 'dist/index.html',
+    }),
+    livereload('src'),
   ],
 };
