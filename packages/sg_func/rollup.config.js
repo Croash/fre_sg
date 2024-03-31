5a61c00cedebbbdbdb885fa70e08ad137a9d168d
shaozelin <1327914853@qq.com>
2024-03-31 17:30:30 +0800

    chore: sg_func config update

--- packages/sg_func/rollup.config.js
+++ packages/sg_func/rollup.config.js
@@ -3,27 +3,53 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-export default {
-  input: 'src/index.js',
-  // TypeScript 入口文件路径
-  // TypeScript 入口文件路径
-  output: [
-    {
-      file: 'dist/index.esm.js',
-      format: 'es', // ES Modules 格式
-      sourcemap: true,
-      //是否生成 sourcemap
+// import pkg from './package.json'
+
+const output = [
+  {
+    format: 'cjs', // 输出格式为 CommonJS
+    dir: 'dist/cjs', // CommonJS 格式的输出目录路径
+    preserveModules: true, // 保持目录结构不变
+  },
+  {
+    format: 'es', // 输出格式为 ES 模块
+    dir: 'dist/esm', // ES 模块格式的输出目录路径
+    preserveModules: true, // 保持目录结构不变
+  }
+]
+
+const cfgList = [
+  {
+    output: {
+      format: 'cjs', // 输出格式为 CommonJS
+      dir: 'dist/cjs', // CommonJS 格式的输出目录路径
+      preserveModules: true, // 保持目录结构不变
+			preserveModulesRoot: 'src'
     },
-    {
-      file: 'dist/index.cjs.js',
-      format: 'cjs' // CommonJS 格式
-    }
-  ],
+    outDir: 'dist/cjs',
+  },
+  {
+    output: {
+      format: 'es', // 输出格式为 ES 模块
+      dir: 'dist/esm', // ES 模块格式的输出目录路径
+      preserveModules: true, // 保持目录结构不变
+      preserveModulesRoot: 'src'
+    },
+    outDir: 'dist/esm',
+  },
+]
+
+const getCommonCfg = ({ outDir, }) => ({
+  input: 'src/index.js',
+  output: [],
   plugins: [
     nodeResolve(), // 解析第三方模块
     commonjs(),
     // 将 CommonJS 模块转换为 ES6
-    typescript(), // 使用 TypeScript 编译代码
+    typescript({
+      tsconfig: 'tsconfig.json', // TypeScript 配置文件路径
+      outDir, // TypeScript 输出目录，应与 Rollup 的输出目录一致
+    }), // 使用 TypeScript 编译代码
     babel({
       // 使用 babel 转换代码
       babelHelpers: 'bundled',
@@ -32,4 +58,9 @@ export default {
     }),
     terser() // 压缩代码 
   ]
-};
+});
+
+export default cfgList.map(({ output, outDir }) => ({
+  ...getCommonCfg({ outDir }),
+  output,
+}))
