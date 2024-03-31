e1fe9d5f6c52adbfd84462996a0b0042945eb3a3
shaozelin <1327914853@qq.com>
2024-03-31 18:58:04 +0800

    chore: update apps/github-pages

--- apps/github-pages/src/app.jsx
+++ apps/github-pages/src/app.jsx
@@ -1,14 +1,10 @@
 // src/App.jsx
-import React, { h } from 'react'; // 替换成你的 React like 库的引入方式
+import * as React from 'react'
+import { h } from 'react'; // 替换成你的 React like 库的引入方式
 
 // 示例组件
-function App() {
-    return (
-        <div>
-            <h1>Hello, React Like Library!</h1>
-            <p>This is a sample component created with your React like library.</p>
-        </div>
-    );
+const App = () => {
+    return <div>hello world</div>;
 }
 
 export { App };
