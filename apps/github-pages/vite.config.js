d5b085dd0074b0bedd54c69667195e4432cebe24
shaozelin <1327914853@qq.com>
2024-04-16 00:51:07 +0800

    fix: gh pages base url

--- apps/github-pages/vite.config.js
+++ apps/github-pages/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
+  // gh-pages base
+  base: "/fre_sg/",
   plugins: [reactRefresh()],
   resolve: {
     alias: {
-      // gh-pages base
-      base: "/fre_sg/",
       // 将 React-like 库设置为 react 别名
       'react': 'fre_sg',
     },
