6ecf1ac3a4b63c5dbd769d1517a9b28fbb1e4758
shaozelin <1327914853@qq.com>
2024-04-17 11:27:02 +0800

    feat: add tailwind

--- apps/github-pages/src/app.jsx
+++ apps/github-pages/src/app.jsx
@@ -1,12 +1,15 @@
 // src/App.jsx
 import * as React from 'react'
 import { useState } from 'react'; // 替换成你的 React like 库的引入方式
+import './index.css'
 
 // 示例组件
 const App = () => {
   const [num, setNum] = useState(0)
-  return <div>
-    hello fre_sg
+  return <div className='text-center'>
+    <h1 className="text-3xl font-bold underline">
+        Hello fre_sg!
+    </h1>
     <button onClick={() => setNum(num + 1)}>{num}</button>
   </div>
 }
