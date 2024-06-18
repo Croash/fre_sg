c6a9110008bdd9ef67306963133eb2a0518c9d40
shaozelin <1327914853@qq.com>
2024-06-18 14:47:34 +0800

    fix: update contract

--- apps/testERC20-contract-interact/src/app.jsx
+++ apps/testERC20-contract-interact/src/app.jsx
@@ -1,13 +1,16 @@
 // src/App.jsx
 import * as React from 'react'
 import { useState, useEffect } from 'react'; // 替换成你的 React like 库的引入方式
-import { getBalance } from './contractInteraction';
+import { getBalance, mint, burn } from './contractInteraction';
 import './index.css'
+
+const reg = /^\d+(\.\d+)?$/
 
 // 示例组件
 const App = () => {
   const [balance, setBalance] = useState('');
   const [account, setAccount] = useState('');
+  const [amount, setAmount] = useState(0);
   const [errorMessage, setErrorMessage] = useState('');
 
   async function _getBalance() {
@@ -18,6 +21,22 @@ const App = () => {
     } catch (error) {
       console.log('error', error);
     }
+  }
+
+  async function _mint() {
+    if (!reg.test(amount)) {
+      alert('not a valid value')
+      return
+    }
+    mint({ account, amount })
+  }
+
+  async function _burn() {
+    if (!reg.test(amount)) {
+      alert('not a valid value')
+      return
+    }
+    burn({ account, amount })
   }
 
   useEffect(() => {
@@ -32,13 +51,18 @@ const App = () => {
 
   return <div className='text-center'>
     <h1 className="text-3xl font-bold underline">
-        Hello fre_sg!
+        Hello testERC20!
     </h1>
     {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-    <div>account:<input onChange={e => setAccount(e.target.value)}></input><button onClick={e => {
-      _getBalance()
-    }}>获取balance</button></div>
-    <div>balance:{balance}</div>
+    <div>account:<input onChange={e => setAccount(e.target.value)}></input>
+    <div>amount:<input onChange={e => setAmount(e.target.value)}></input>
+    <button onClick={e => {
+      _burn()
+    }}>burn</button></div>
+    <button onClick={e => {
+      _mint()
+    }}>mint</button></div>
+    {/* <div>balance:{balance}</div> */}
   </div>
 }
 
--- apps/testERC20-contract-interact/src/contractInteraction.js
+++ apps/testERC20-contract-interact/src/contractInteraction.js
@@ -7,6 +7,7 @@ async function getContract() {
     // const provider = new ethers.JsonRpcProvider('http://localhost:7545')
     const provider = new ethers.BrowserProvider(window.ethereum)
     const signer = await provider.getSigner();
+    console.log(signer)
     const contract = new ethers.Contract(contractAddress, abi, signer);
     window.contract = contract
     return contract;
@@ -25,4 +26,19 @@ async function getBalance(account) {
   return balance // getBalance
 }
 
-export { getContract, getBalance };
+async function mint({ account, amount }) {
+  const contract = await getContract();
+  console.log('contract:', contract.mint)
+  const status = await contract.mint(account, ethers.parseEther(amount))
+  console.log('status', status);
+  // return balance // getBalance
+}
+
+async function burn({ account, amount }) {
+  const contract = await getContract();
+  const status = await contract.burn(account, ethers.parseEther(amount))
+  console.log('status', status);
+  // return balance // getBalance
+}
+
+export { getContract, getBalance, mint, burn };
